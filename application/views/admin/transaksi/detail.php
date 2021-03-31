299939889417d388291078404834f571acabef99
idrisrm <55520214+idrisrm@users.noreply.github.com>
2021-04-01 00:55:09 +0700

    Update detail.php

--- application/views/admin/transaksi/detail.php
+++ application/views/admin/transaksi/detail.php
@@ -50,7 +50,7 @@ $dataWa = substr($dataPenerima['wa'], 1);
                 <div class="row mt-3">
                   <div class="form-group col-lg-6 col-sm-6">
                     <h3>Nomor WhatsApp</h3>
-                    <p><a href="https://wa.me/+62<?= $dataWa ?>"><?= $dataPenerima['wa'] ?></a></p>
+                    <p><a href="https://api.whatsapp.com/send?phone=62<?= $dataWa?>&text=Hai%20kakak!%20Saya%20dari%20Admin%20Mak%20Enak%20ingin%20menginformasikan%20bahwa%20pesanan%20yang%20anda%20buat%20dengan%20data%20berikut%3A%20%0A1.%20nama%20rekening%20pengirim%20<?= $dataPenerima['namaPengirim']?>%0A2.%20tanggal%20pemesanan%20<?= $dataPenerima['tanggalTransaksi']?>%20%0A3.%20total%20pembayaran%20<?= number_format($dataPenerima['totalBayar'], 2, ",", ".") ?>"><?= $dataPenerima['wa'] ?></a></p>
                   </div>
                   <div class="form-group col-lg-6 col-sm-6">
                     <h3>Alamat</h3>
