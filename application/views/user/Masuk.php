e36cd1d3d530dca6fe6c43f14b823fe56e7fa162
idrisrm <55520214+idrisrm@users.noreply.github.com>
2021-04-06 11:35:32 +0700

    coba

--- application/views/user/Masuk.php
+++ application/views/user/Masuk.php
@@ -1,68 +1,59 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html>
 
 <head>
-  <?php $this->load->view('user/_partials/head.php') ?>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no">
+  <title>Hostify</title>
+  <link rel="stylesheet" type="text/css" href="<?= base_url('css/auth/bootstrap.min.css') ?>">
+  <link rel="stylesheet" type="text/css" href="<?= base_url('css/auth/bootstrap-slider.min.css') ?>">
+  <link rel="stylesheet" type="text/css" href="<?= base_url('css/auth/fontawesome-all.min.css') ?>">
+  <link rel="stylesheet" type="text/css" href="<?= base_url('css/auth/slick.css') ?>">
+  <link rel="stylesheet" type="text/css" href="<?= base_url('css/auth/style.css') ?>">
+  <link rel="stylesheet" type="text/css" href="<?= base_url('css/auth/custom.css') ?>">
 </head>
 
-<body>
-
-  <?php $this->load->view('user/_partials/static_color_navbar.php') ?>
-
-  <main id="main">
-
-    <section id="sign-in" class="sign-in">
-
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-lg-5">
-            <div class="card shadow-lg border-0 rounded-lg mt-15">
-              <div class="card-header justify-content-center">
-                <h5>Silahkan Login, Untuk Memulai Berbelanja.</h5>
-              </div>
-              <div class="card-body">
-                <div class="col">
-                <?php echo $this->session->flashdata('message') ?>
-                  <form action="" method="post">
-                    <div class="col-md-12"><label class=" mb-1" for="password">Email</label>
-                      <input type="email" class="form-control mb-3" name="email" placeholder="Email" required>
-                    </div>
-                    <div class="col-md-12"><label class=" mb-1" for="password">Password</label>
-                      <input class="form-control" id="password" name="password" type="password" placeholder="Password" />
-                    </div>
-                    <div class="form-group d-flex align-items-center justify-content-between mt-4 mb-0">
-                      <button type="submit" class="btn btn-primary">
-                        Sign In
-                      </button>
-                    </div>
-                  </form>
-                </div>
-                
-              </div>
-              <div class="card-footer text-center">
-                <div class="small"><a href="<?= base_url('Auth/Daftar') ?>">Belum Punya Akun? Daftar Disini</a></div>
-              </div>
-            </div>
-          </div>
-
-          <div class="copyright col-12">
-            Copyright &copy; <strong> <span><?= SITE_NAME ?></span> </strong>
-            <script>
-              document.write(new Date().getFullYear());
-            </script>
-          </div>
-
+<body class="fullpage">
+  <div id="form-section" class="container-fluid signin">
+    <div class="row">
+      <div class="info-slider-holder">
+        <div class="info-holder">
+          <h6>Mak Enak Jember</h6>
+          <div class="bold-title">Oleh - oleh Khas Jember<br>
+            Untuk Menemani <span>Harimu</span></div>
         </div>
       </div>
-
-    </section>
-
-  </main>
-
-  <!-- <?php $this->load->view('user/_partials/floating_whatsapp.php') ?> -->
-
-  <?php $this->load->view('user/_partials/js.php') ?>
-
+      <div class="form-holder">
+        <div class="menu-holder">
+          <ul class="main-links">
+            <li><a class="normal-link" href="<?= base_url('Auth/Daftar')?>">Belum Punya Akun?</a></li>
+            <li><a class="sign-button" href="<?= base_url('Auth/Daftar')?>">Daftar</a></li>
+          </ul>
+        </div>
+        <div class="signin-signup-form">
+          <div class="form-items">
+            <div class="form-title">Masuk dan mulai berbelanja</div>
+            <form id="signinform" method="POST">
+              <div class="form-text">
+                <input type="text" name="email" placeholder="Masukan Email Anda" required>
+              </div>
+              <div class="form-text">
+                <input type="password" name="password" placeholder="Masukan Password Anda" required>
+              </div>
+              <div class="form-button">
+                <button id="submit" type="submit" class="ybtn ybtn-accent-color">Sign in</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="<?= base_url() ?>js/auth/jquery.min.js"></script>
+  <script src="<?= base_url() ?>js/auth/bootstrap.min.js"></script>
+  <script src="<?= base_url() ?>js/auth/bootstrap-slider.min.js"></script>
+  <script src="<?= base_url() ?>js/auth/slick.min.js"></script>
+  <script src="<?= base_url() ?>js/auth/main.js"></script>
 </body>
 
 </html>
