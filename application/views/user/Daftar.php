163dbdcd690f3107afc68df429c711ba6d232cc4
idrisrm <55520214+idrisrm@users.noreply.github.com>
2021-04-08 10:20:45 +0700

    Update Daftar.php

--- application/views/user/Daftar.php
+++ application/views/user/Daftar.php
@@ -4,7 +4,10 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no">
-  <title>Hostify</title>
+  <title><?= SITE_NAME ?></title>
+  
+  <link href="<?= base_url('img/Icon/Logo.png') ?>" rel="icon" type="image/x-icon">
+  <link href="<?= base_url('img/Icon/Logo.png') ?>" rel="apple-touch-icon">
   <link rel="stylesheet" type="text/css" href="<?= base_url('css/auth/bootstrap.min.css') ?>">
   <link rel="stylesheet" type="text/css" href="<?= base_url('css/auth/bootstrap-slider.min.css') ?>">
   <link rel="stylesheet" type="text/css" href="<?= base_url('css/auth/fontawesome-all.min.css') ?>">
@@ -27,35 +30,36 @@
         <div class="menu-holder">
           <ul class="main-links">
             <li><a class="normal-link" href="<?= base_url('Auth/Daftar') ?>">Sudah Punya Akun?</a></li>
-            <li><a class="sign-button" href="<?= base_url('Auth/Daftar') ?>">Login</a></li>
+            <li><a class="sign-button" href="<?= base_url('Auth') ?>">Login</a></li>
           </ul>
         </div>
         <div class="signin-signup-form">
           <div class="form-items">
             <div class="form-title">Daftar dan dapatkan oleh-oleh kesukaanmu</div>
-            <form id="signupform">
+            <form id="signupform" method="POST">
               <div class="row">
                 <div class="col-md-6 form-text">
                   <input type="text" name="nama" placeholder="Nama Lengkap" required />
                 </div>
                 <div class="col-md-6 form-text">
-                  <input type="text" name="email" placeholder="Email" required />
+                  <input type="text" name="noWa" placeholder="Nomor WhatsApp" required />
                 </div>
               </div>
               <div class="form-text">
-                <input type="text" name="username" placeholder="E-mail Address" required />
+                <input type="text" name="email" placeholder="Email" required />
+              </div>
+              <div class="form-text">
+                <input type="text" name="alamat" placeholder="Alamat Rumah" required />
               </div>
               <div class="form-text">
                 <input type="password" name="password" placeholder="Password" required />
               </div>
-              <div class="form-text text-holder">
-                <span class="text-only">Preferred method of payment.</span>
-                <input type="radio" name="pmethod" class="hno-radiobtn" id="rad1" /><label for="rad1">Paypal</label>
-                <input type="radio" name="pmethod" class="hno-radiobtn" id="rad2" /><label for="rad2">Credit Card</label>
+              <div class="form-text">
+                <input type="password" name="konfirmasiPassword" placeholder="Konfirmasi Password" required />
               </div>
               <div class="form-button">
                 <button id="submit" type="submit" class="ybtn ybtn-accent-color">
-                  Create new account
+                  Daftar
                 </button>
               </div>
             </form>
