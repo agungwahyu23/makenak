9f95a40fc3d1271ec77fa51c56e6e2cb6204a72f
idrisrm <55520214+idrisrm@users.noreply.github.com>
2021-05-10 13:23:56 +0700

    grafik

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -20,10 +20,22 @@ class Dashboard extends CI_Controller
 		$data['totalTransaksiDitolak'] = $this->db->get_where('transaksi', ['status' => 4])->num_rows();
 
 		// data produk terlaris
-		$this->db->join('detailtransaksi', 'detailtransaksi.idProduk = produk.id');
-		$this->db->join('transaksi', 'transaksi.status = 3');
-		$this->db->select('produk.namaProduk, detailtransaksi.jumlahBeli','(SELECT produk.namaProduk SUM(detailtransaksi.jumlahBeli) FROM detailtransaksi WHERE detailtransaksi.idProduk = produk.id) AS jumlahBeli', TRUE);
-		$data['produkTerlaris'] = $this->db->get('produk', 10)->result_array();
+		// $this->db->join('detailtransaksi', 'detailtransaksi.idProduk = produk.id');
+		// $this->db->join('transaksi', 'transaksi.status = 3');
+		// $data['produkTerlaris'] =  $this->db->select('produk.namaProduk, detailtransaksi.jumlahBeli','(SELECT produk.namaProduk SUM(detailtransaksi.jumlahBeli) FROM detailtransaksi WHERE detailtransaksi.idProduk = produk.id) AS totalBeli', TRUE);
+
+
+
+		// $this->db->join('produk', 'produk.id = detailtransaksi.idProduk');
+		$data['produkTerlaris'] = $this->db->select('idProduk, namaProduk, SUM(jumlahBeli) as jumlahBeli')
+                 ->from('detailtransaksi')
+				 ->join('produk', 'produk.id = detailtransaksi.idProduk', 'LEFT')
+                 ->order_by('jumlahBeli','desc')
+                 ->limit(10)
+                 ->group_by('idProduk')
+                 ->get()->result_array();
+		// $data['produkTerlaris'] = $this->db->get('produk', 10)->result_array();
+		// var_dump($data['produkTerlaris']);die;
 		
 
 		$pengunjung =  $this->db->query("SELECT COUNT(id_pengunjung) as count,MONTHNAME(tanggal) as month_name FROM pengunjung WHERE YEAR(tanggal) = '" . date('Y') . "' GROUP BY YEAR(tanggal), MONTH(tanggal)")->result_array();
